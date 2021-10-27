2d25e714837c0f4298d5eddc615e141121d78515
xjzsq <sdxjzsq@126.com>
2021-10-27 08:30:17 +0800

    add comment

--- app.js
+++ app.js
@@ -10,41 +10,41 @@ const secret = 'chtholly'
 const app = new Koa()
 const router = new Router()
 app.use(bodyParser())
-const errbody = {
+const errbody = {                                                                           // 参数错误统一返回 errbody
     message: '参数错误',
     code: -1
 }
 app
     .use(jwtKoa({ secret }).unless({
-        path: [/^\/createsalt/, /^\/register/, /^\/getsalt/, /^\/login/] //数组中的路径不需要通过jwt验证
+        path: [/^\/createsalt/, /^\/register/, /^\/getsalt/, /^\/login/]                    //数组中的路径不需要通过jwt验证
     }))
 router
-    .get('/createsalt', async (ctx, next) => {
+    .get('/createsalt', async (ctx, next) => {                                              // 新用户注册创建salt
         const user = ctx.request.query;
         if (user && user.name) {
             let hasRegistration = false;
-            let userinfo = JSON.parse(fs.readFileSync('./user.json', 'utf-8'));
+            let userinfo = JSON.parse(fs.readFileSync('./user.json', 'utf-8'));             // 读取用户信息
             for (let i in userinfo) {
-                if (userinfo[i] && userinfo[i].name && userinfo[i].name === user.name) {
+                if (userinfo[i] && userinfo[i].name && userinfo[i].name === user.name) {    // 是否已经注册
                     hasRegistration = true;
                     break;
                 }
             }
-            if (hasRegistration) {
+            if (hasRegistration) {                                                          // 已经注册则返回已存在
                 ctx.body = {
                     message: '用户名已存在',
                     code: -1
                 }
             } else {
-                let salt = Math.random().toString(36).slice(-8);
-                let systeminfo = JSON.parse(fs.readFileSync('./system.json', 'utf-8'));
+                let salt = Math.random().toString(36).slice(-8);                            // 随机生成salt
+                let systeminfo = JSON.parse(fs.readFileSync('./system.json', 'utf-8'));     // 读取系统配置
                 userinfo.push({
-                    "id": ++systeminfo.userNum,
+                    "id": ++systeminfo.userNum,                                             // 增加系统总人数
                     "name": user.name,
                     "salt": salt
                 })
-                fs.writeFileSync('./system.json', JSON.stringify(systeminfo));
-                fs.writeFileSync('./user.json', JSON.stringify(userinfo));
+                fs.writeFileSync('./system.json', JSON.stringify(systeminfo));              // 写入系统信息
+                fs.writeFileSync('./user.json', JSON.stringify(userinfo));                  // 写入用户信息
                 ctx.body = {
                     message: 'salt 生成成功！',
                     code: 1,
